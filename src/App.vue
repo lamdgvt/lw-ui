08d181f0a9a8a6a4f124162e655c4840899f3d04
lin <491801318@qq.com>
2018-12-17 22:02:32 +0800

    feat:lw-ui 初始化

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="2">
+        <lw-button>确认</lw-button>
+      </el-col>
+      <el-col :span="2">
+        <lw-button>清除</lw-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { Row, Col } from "element-ui";
+import LwButton from "./enhance/LwButton";
+
+export default {
+  name: 'app',
+  components: {
+    ElRow: Row,
+    ElCol: Col,
+    LwButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/enhance/LwButton.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+export default {
+  name: "LwButton",
+  data: function() {
+    return {};
+  },
+  props: {
+    value: String,
+    type: {
+      type: String,
+      default: "default"
+    }
+  },
+  render: function(createElement) {
+    console.log(createElement);
+    return createElement("div", "1234");
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.lw_button {
+  button {
+    padding: 3px 12px;
+    box-sizing: border-box;
+    font-size: 12px;
+    white-space: nowrap;
+    text-align: center;
+    transition: 0.1s;
+    font-weight: 500;
+    border-radius: 2px;
+    cursor: pointer;
+    outline: none;
+    border: 1px solid transparent;
+    color: #fff;
+  }
+  .default {
+    background-color: #1890ff;
+  }
+  .default:hover {
+    background-color: #40a9ff;
+  }
+  .default:active {
+    background-color: #096dd9;
+  }
+}
+</style>
+
