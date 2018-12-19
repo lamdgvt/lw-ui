1d6eb811f9614f4469eeb205d5c8f2c1df2aab88
lin <491801318@qq.com>
2018-12-19 22:54:22 +0800

    feat: 简易封装LwButton 2018-12-19

--- src/enhance/LwButton.vue
+++ src/enhance/LwButton.vue
@@ -1,47 +1,110 @@
+<template>
+  <div class="lw_button">
+    <component
+      :is="tagName"
+      :type="type"
+      :class="classes"
+      :disabled="disabled"
+      @click="clickEvent"
+      @dblclick="dblclickEvent"
+    >
+      <slot></slot>
+    </component>
+  </div>
+</template>
+
+
 <script lang="ts">
+import { isString } from "lodash";
 export default {
   name: "LwButton",
   data: function() {
-    return {};
+    return {
+      tagName: "button"
+    };
   },
   props: {
-    value: String,
     type: {
       type: String,
       default: "default"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
   },
-  render: function(createElement) {
-    console.log(createElement);
-    return createElement("div", "1234");
+  computed: {
+    classes: function() {
+      return this.type && isString(this.type) ? this.type : "default";
+    }
+  },
+  methods: {
+    clickEvent: function() {
+      this.$emit("click", event);
+    },
+    dblclickEvent: function() {
+      this.$emit("dblclick", event);
+    }
   }
 };
 </script>
 
 <style lang="less" scoped>
+@import "../skins/color.less";
 .lw_button {
-  button {
+  padding: 2px;
+  button,
+  a {
     padding: 3px 12px;
     box-sizing: border-box;
     font-size: 12px;
     white-space: nowrap;
     text-align: center;
-    transition: 0.1s;
+    transition: 0.3s;
     font-weight: 500;
     border-radius: 2px;
     cursor: pointer;
     outline: none;
     border: 1px solid transparent;
-    color: #fff;
+    color: @White;
   }
   .default {
-    background-color: #1890ff;
+    background-color: @Blue;
   }
   .default:hover {
-    background-color: #40a9ff;
+    background-color: @BlueHover;
   }
   .default:active {
-    background-color: #096dd9;
+    background-color: @BlueActive;
+  }
+  .success {
+    background: @Green;
+  }
+  .success:hover {
+    background: @GreenHover;
+  }
+  .success:active {
+    background: @GreenActive;
+  }
+  .warn {
+    background: @Red;
+  }
+  .warn:hover {
+    background: @RedHover;
+  }
+  .warn:active {
+    background: @RedActive;
+  }
+  .clear {
+    background: @White;
+    border-color: @Gray;
+    color: @Blue;
+  }
+  .clear:hover {
+    border-color: @Blue;
+  }
+  .clear:active {
+    background-color: @WhiteActive;
   }
 }
 </style>
